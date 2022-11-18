862d245971ce86b4a3aa1cecf740ffbbecca2ab1
arcelioM <arceliomonte15@gmail.com>
2022-11-18 14:01:23 -0500

    Tabla de direccion, rol, usuario y rolUsuario fue creada

--- Script BD/Creacion de tablas.sql
+++ Script BD/Creacion de tablas.sql
@@ -35,3 +35,55 @@ CREATE TABLE corregimiento(
     CONSTRAINT FK_corregimiento_distrito_id_distritos FOREIGN KEY (distrito_id) REFERENCES distrito(id_distrito),
     CONSTRAINT FK_corregimiento_estado_idEstado FOREIGN KEY (estado_id) REFERENCES estado(id_estado)
 );
+
+# TENDRA LAS DIRECCIONES DISPONIBLES 
+
+CREATE TABLE direccion (
+    id_direccion INT AUTO_INCREMENT PRIMARY KEY,
+    corregimiento_id INT NOT NULL,
+    direccion_especifica VARCHAR(40) DEFAULT "no registrado" NOT NULL,
+    estado_id INT NOT NULL DEFAULT 1,
+    CONSTRAINT FK_direccion_corregimiento FOREIGN KEY (corregimiento_id) REFERENCES corregimiento(id_corregimiento),
+	CONSTRAINT FK_direccion_estado_idEstado FOREIGN KEY (estado_id) REFERENCES estado(id_estado)
+);
+
+# TENDRA LOS ROLES DE USUARIO DISPONIBLE
+CREATE TABLE rol(
+	id_rol INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(30) NOT NULL,
+    estado_id INT NOT NULL,
+    fechaCreacion DATETIME NOT NULL DEFAULT NOW(),
+    CONSTRAINT FK_rol_estado_idEstado FOREIGN KEY (estado_id) REFERENCES estado(id_estado)
+);
+
+# TENDRA TODOS LOS USUARIOS QUE SE REGISTREN
+CREATE TABLE usuario(
+	id_usuario INT PRIMARY KEY,
+    usuario_nombre VARCHAR(30) NOT NULL ,
+    nombre VARCHAR(30) NOT NULL,
+    apellido VARCHAR(30) NOT NULL,
+    contraseña VARCHAR(200) NOT NULL,
+    email  varchar(50) NOT NULL,
+    direccion_id INT NOT NULL,
+    telefono_1 INT NOT NULL,
+    telefono_2 INT NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    estado_id INT NOT NULL,
+    fechaCreacion DATETIME NOT NULL DEFAULT NOW(),
+    CONSTRAINT FK_usuario_estado_idEstado FOREIGN KEY (estado_id) REFERENCES estado(id_estado),
+    CONSTRAINT FK_usuario_direccion_idDireccion FOREIGN KEY(direccion_id) REFERENCES direccion(id_direccion),
+    CONSTRAINT UN_usuario_usuarioNombre UNIQUE (usuario_nombre)
+);
+
+# TENDRA LOS ROLES DE CADA USUARIO REGISTRADO
+
+CREATE TABLE usuarioRol(
+	id_usuarioRol INT PRIMARY KEY AUTO_INCREMENT,
+    rol_id INT NOT NULL,
+    usuario_id INT NOT NULL,
+    estado_id INT NOT NULL,
+    fechaCreacion DATETIME NOT NULL DEFAULT NOW(),
+    CONSTRAINT FK_usuarioRol_estado_idEstado FOREIGN KEY (estado_id) REFERENCES estado(id_estado),
+    CONSTRAINT FK_usuarioRol_usuario_idUsuario FOREIGN key (usuario_id) REFERENCES usuario(id_usuario),
+    CONSTRAINT FK_usuarioRol_rol_idRol FOREIGN KEY (rol_id) REFERENCES rol (id_rol)
+);
