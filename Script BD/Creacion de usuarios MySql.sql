1a7db600aacfc24f31556b88bd1f55e24e8996e6
arcelioM <arceliomonte15@gmail.com>
2022-11-18 17:27:09 -0500

    Privilegios asignados a usuario de BD  Cliente

--- Script BD/Creacion de usuarios MySql.sql
+++ Script BD/Creacion de usuarios MySql.sql
@@ -14,7 +14,23 @@ FLUSH PRIVILEGES;
 
 # USUARIO QUE UTILIZARAN CLIENTES
 CREATE USER 'cliente'@'localhost' IDENTIFIED BY 'hola';
--- LOS PRIVILEGIOS LO ASIGNARA DESPUES DE CREAR LAS TABLAS
 
-#GRANT SELECT, INSERT, UPDATE, EXECUTE ON ventas.* TO 'administrador'@'localhost';
-#FLUSH PRIVILEGES;
+GRANT EXECUTE ON ventas.* TO 'cliente'@'localhost';
+GRANT SELECT  ON ventas.categoria TO 'cliente'@'localhost';
+GRANT SELECT  ON ventas.corregimiento TO 'cliente'@'localhost';
+GRANT SELECT,INSERT,UPDATE  ON ventas.direccion TO 'cliente'@'localhost';
+GRANT SELECT  ON ventas.distrito TO 'cliente'@'localhost';
+GRANT SELECT  ON ventas.estado TO 'cliente'@'localhost';
+GRANT SELECT,INSERT  ON ventas.factura TO 'cliente'@'localhost';
+GRANT INSERT  ON ventas.facturapago TO 'cliente'@'localhost';
+GRANT INSERT  ON ventas.pago TO 'cliente'@'localhost';
+GRANT SELECT,INSERT,UPDATE  ON ventas.pedido TO 'cliente'@'localhost';
+GRANT SELECT,INSERT,UPDATE  ON ventas.pedidoproducto TO 'cliente'@'localhost';
+GRANT SELECT  ON ventas.producto TO 'cliente'@'localhost';
+GRANT SELECT  ON ventas.provincia TO 'cliente'@'localhost';
+GRANT SELECT  ON ventas.tipopago TO 'cliente'@'localhost';
+GRANT SELECT  ON ventas.tipopedido TO 'cliente'@'localhost';
+GRANT SELECT,INSERT,UPDATE  ON ventas.usuario TO 'cliente'@'localhost';
+GRANT SELECT,INSERT  ON ventas.usuariorol TO 'cliente'@'localhost';
+
+FLUSH PRIVILEGES;
