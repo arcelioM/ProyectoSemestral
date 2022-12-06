1fcbd3682f4e7e3f81e991c979f2dc382e946eff
arcelioM <arceliomonte15@gmail.com>
2022-12-05 22:45:07 -0500

    Modificacion en SCRIPT para Procedimiento almacenado para actualizar usuario

--- Script BD/SP_actualizarUsuario.sql
+++ Script BD/SP_actualizarUsuario.sql
@@ -1,8 +1,7 @@
 CREATE DEFINER=`user_ventas`@`localhost` PROCEDURE `SP_actualizarUsuario`(
-	IN idUsuario INT,IN usuario VARCHAR(30),IN nombre VARCHAR(30), IN apellido VARCHAR(30),
-    IN contraseñaA VARCHAR(200),IN nuevaContraseña VARCHAR(200) ,IN email VARCHAR(50),IN imagen VARCHAR(75),
-    IN idDireccion INT,IN idCorregimiento INT,
-    IN direcionEspecifica VARCHAR(40),IN telefono1 INT,
+	IN idUsuario INT,IN usuarioArg VARCHAR(30),IN nombreArg VARCHAR(30), IN apellidoArg VARCHAR(30),
+    IN contraseñaA VARCHAR(200),IN nuevaContraseña VARCHAR(200) ,IN emailArg VARCHAR(50),IN imagenArg VARCHAR(75),
+	IN telefono1 INT,
     IN telefono2 INT, IN fechaNacimiento VARCHAR(30)
 )
 BEGIN
@@ -16,20 +15,23 @@ BEGIN
     END;
 
     START TRANSACTION;
+		
 		SET idUsuarioExits = (SELECT count(id_usuario) FROM usuario WHERE id_usuario=idUsuario);
 		IF(idUsuarioExits=0) THEN
 			SIGNAL SQLSTATE '02001' SET MYSQL_ERRNO = ER_SIGNAL_NOT_FOUND;
         END IF;
         
-        UPDATE direccion SET corregimiento_id=idCorregimiento,direccion_especifica=direcionEspecifica 
-        WHERE id_direccion =  idDireccion;
         
-        UPDATE usuario SET usuario_nombre=usuario,nombre =nombre,apellido = apellido, email=email,telefono_1=telefono1,telefono_2=telefono2,fecha_nacimiento=fechaNacimiento
+        UPDATE usuario SET usuario_nombre=usuarioArg,nombre =nombreArg,apellido = apellidoArg, email=emailArg,telefono_1=telefono1,telefono_2=telefono2,fecha_nacimiento=fechaNacimiento
         WHERE id_usuario=idUsuario;
         SET contraseñaO =(SELECT contraseña FROM usuario  WHERE id_usuario = idUsuario);
-        
+        SELECT SHA1(contraseñaA) INTO contraseñaA;
         IF contraseñaO=contraseñaA THEN
-			UPDATE usuario SET contraseña=nuevaContraseña WHERE id_usuario = idUsuario;
+			UPDATE usuario SET contraseña=SHA1(nuevaContraseña) WHERE id_usuario = idUsuario;
+        END IF;
+        
+        IF imagenArg <> null OR imagenArg <> '' THEN
+			UPDATE usuario SET imagen=imagenArg WHERE id_usuario = idUsuario;
         END IF;
 		
         SELECT 1 as result;
