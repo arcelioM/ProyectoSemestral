060bc78a001245154151d24f088ece7467bac02d
arcelioM <arceliomonte15@gmail.com>
2022-12-07 02:19:13 -0500

    Modificacion en funcionalidad de activar y eliminar usuario

--- view/Controler/controlesTotal/controlMenuAdmin.js
+++ view/Controler/controlesTotal/controlMenuAdmin.js
@@ -87,7 +87,7 @@ $(document).ready(function () {
 
     $(document).on('click', '.eliminarUser', function () {
         let element = $(this)[0].parentElement.parentElement;    //aquí se almacena toda la fila por medio de la propiepdad parentElement y va de "td" al padre "tr"
-        let id = $(element).attr('Id_Homework');  //aquí se almacena el id del botón selecccionado por medio de fila padre
+        let id = $(element).attr('Id_Homeworkoko');  //aquí se almacena el id del botón selecccionado por medio de fila padre
 
         $.post('http://localhost/ProyectoSemestral/view/phpPruebas/VariabbleUserEliminar.php?', { id }, function (response) {
             //let valor= response.idEliminarTablaU;
@@ -169,7 +169,7 @@ $(document).ready(function () {
                     </button>`;
                     } else {
                         tipoUser = `
-                        <buttontype="button" class="btn btn-danger  activarUser" data-bs-toggle="modal" data-bs-target="#ModalactivarUserAux"> <svg xmlns="http://www.w3.org/2000/svg" width="5%" height="5%" fill="currentColor" class="bi bi-person-fill-slash" viewBox="0 0 16 16">
+                        <button type="button" class="btn btn-danger  activarUser" data-bs-toggle="modal" data-bs-target="#ModalactivarUserAux"> <svg xmlns="http://www.w3.org/2000/svg" width="5%" height="5%" fill="currentColor" class="bi bi-person-fill-slash" viewBox="0 0 16 16">
                         <path d="M13.879 10.414a2.501 2.501 0 0 0-3.465 3.465l3.465-3.465Zm.707.707-3.465 3.465a2.501 2.501 0 0 0 3.465-3.465Zm-4.56-1.096a3.5 3.5 0 1 1 4.949 4.95 3.5 3.5 0 0 1-4.95-4.95ZM11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm-9 8c0 1 1 1 1 1h5.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.544-3.393C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4Z"/>
                       </svg>
                         Eliminado
@@ -231,7 +231,7 @@ $(document).ready(function () {
                             <td ><img src="http://localhost/ProyectoSemestral/view/imagenes/${prodctoGe.imagen}" class="img"></td>
                             <td >${prodctoGe.descripcion}</td> 
                             <td >${prodctoGe.precio}</td>
-                            <td >${prodctoGe.precio}</td>
+                            <td >${prodctoGe.cantidad}</td>
                             </tr>
                         `;
 
